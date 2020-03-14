689d10433f66711ce9186248e3544b3173ca4353
Mario Pérez <mario.perez17@alu.uclm.es>
2020-03-14 18:59:47 +0100

    Implementacion envio separado en filas y columnas

--- toroidal.c
+++ toroidal.c
@@ -12,10 +12,12 @@
 #define TRUE        1
 #define FALSE       0
 
+/* For neighbors */
 #define NORTH       1
 #define SOUTH       2
 #define EAST        3
 #define WEST        4
+#define N_NEIGHBORS 4
 
 /* For data checks */
 #define LENGTH_MSG "length"
@@ -74,21 +76,80 @@ void send_data(double *data){
     free(data);
 }
 
-void toroidal_neighbors(int rank){
+void toroidal_neighbors(int rank, int neighbors[]){
+    int row = rank / L;
+    int column = rank % L;
 
+    switch (row){
+        /* 
+            1 2
+            | |
+            3 4
+        */
+        case 0: /* Lower row */
+            neighbors[NORTH] = rank + L;
+            neighbors[SOUTH] = (L-1) * L + rank;
+            break;
+        case L-1: /* Uppper row */
+            neighbors[NORTH] = rank % L;
+            neighbors[SOUTH] = rank - L; 
+            break;
+        default: /* Central row */
+            neighbors[NORTH] = rank + L;
+            neighbors[SOUTH] = rank - L; 
+            break;
+    }
+
+    switch(column){
+        /* 
+            1 -- 2
+            3 -- 4
+        */
+        case 0: /* Left column */
+            neighbors[WEST] = (L * (row+1))-1;
+            neighbors[EAST] = rank + 1;
+            break;
+        case L-1: /* Right column */
+            neighbors[WEST] = rank -1;
+            neighbors[EAST] = row * L;
+            break;
+        default: /* Central column */
+            neighbors[WEST] = rank -1;
+            neighbors[EAST] = rank + 1;  
+            break;
+    }
 }
 
 void calculate_min(int rank, double num){
-    int neighbors[L];
+    int neighbors[N_NEIGHBORS];
+    double his_num;
+    int i;
 
+    toroidal_neighbors(rank, neighbors);
+
+    for(i=1; i < L; i++){
+        MPI_Send(&num, 1, MPI_DOUBLE, neighbors[SOUTH], 1, MPI_COMM_WORLD);
+        MPI_Recv(&his_num,1,MPI_DOUBLE,neighbors[NORTH],1,MPI_COMM_WORLD,NULL);
+        num = MIN(num,his_num);
+    }
+
+    for(i=1; i < L; i++){
+        MPI_Send(&num, 1, MPI_DOUBLE, neighbors[EAST], 1, MPI_COMM_WORLD);
+        MPI_Recv(&his_num,1,MPI_DOUBLE,neighbors[WEST],1,MPI_COMM_WORLD,NULL);
+        num = MIN(num,his_num);
+    }
+
+    if(rank == 0){
+        printf("\nThe minimum number is: %.2f\n",num);
+    }
+    
 }
 
 int main(int argc, char *argv[]){
-    double *data = malloc(MAX_ITEMS*sizeof(double));
+    double *data = malloc(MAX_ITEMS * sizeof(double));
     int length;
     int rank, size;
     double num;
-
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
@@ -110,9 +171,9 @@ int main(int argc, char *argv[]){
         /* Wait the number */
         MPI_Recv(&num, 1, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, NULL);
         printf("Rank %d => %.2f\n",rank,num);
+        calculate_min(rank, num);
     }
     
-
     MPI_Finalize();
 
     return EXIT_SUCCESS;
